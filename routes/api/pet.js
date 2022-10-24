ce4f39768da5ace12f61977da24c8706e29c47db
joshua-mandel <89474480+joshua-mandel@users.noreply.github.com>
2022-10-24 16:20:07 -0500

    update

--- routes/api/pet.js
+++ routes/api/pet.js
@@ -3,7 +3,7 @@ const debugError = require('debug')('app:error');
 const express = require('express');
 const { nanoid } = require('nanoid');
 const dbModule = require('../../database');
-const { newId } = require('../../database');
+const { newId, connect } = require('../../database');
 const Joi = require('joi');
 const validId = require('../../middleware/validId');
 const validBody = require('../../middleware/validBody');
@@ -34,15 +34,71 @@ const updatePetSchema = Joi.object({
 //create a router
 const router = express.Router();
 
-
-
 //define routes
 
 // find all
 router.get('/api/pet/list', async (req, res, next) => {
   try {
-    const pets = await dbModule.findAllPets();
-    res.json(pets);
+    let { keywords, species, minAge, maxAge, sortBy, pageNumber, pageSize } = req.query;
+
+    debug(req.query);
+    minAge = parseInt(minAge);
+    maxAge = parseInt(maxAge);
+
+    // match stage
+    const match = {};
+    if (keywords) {
+      match.$text = { $search: keywords };
+    }
+    if (species) {
+      match.species = { $eq: species };
+    }
+    if (minAge && maxAge) {
+      match.age = { $gte: minAge, $lte: maxAge };
+    } else if (minAge) {
+      match.age = { $gte: minAge };
+    } else if (maxAge) {
+      match.age = { $lte: maxAge };
+    }
+
+    // sort stage
+    let sort = { name: 1, createdDate: 1 };
+    switch (sortBy) {
+      case 'species': sort = { species: 1, name: 1, createdDate: 1 }; break;
+      case 'species_desc': sort = { species: -1, name: -1, createdDate: -1 }; break;
+      case 'name': sort = { name: 1, createdDate: 1 }; break;
+      case 'name_desc': sort = { name: -1, createdDate: -1 }; break;
+      case 'age': sort = { age: 1, createdDate: 1 }; break;
+      case 'age_desc': sort = { age: -1, createdDate: -1 }; break;
+      case 'gender': sort = { gender: 1, name: 1, createdDate: 1 }; break;
+      case 'gender_desc': sort = { gender: -1, name: -1, createdDate: -1 }; break;
+      case 'oldest': sort = { createdDate: 1 }; break;
+      case 'newest': sort = { createdDate: -1 }; break;
+    }
+
+    // project stage
+    const project = { species: 1, name: 1, age: 1, gender: 1 };
+
+    // skip and limit stages
+    pageNumber = parseInt(pageNumber) || 1;
+    pageSize = parseInt(pageSize) || 5;
+    const skip = (pageNumber - 1) * pageSize;
+    const limit = pageSize;
+
+    // pipeline
+    const pipeline = [
+      { $match: match },
+      { $sort: sort },
+      { $project: project },
+      { $skip: skip },
+      { $limit: limit }
+    ];
+
+    const db = await connect();
+    const cursor = db.collection('pets').aggregate(pipeline);
+    const results = await cursor.toArray();
+
+    res.json(results);
   } catch (err) {
     next(err);
   }
@@ -75,10 +131,10 @@ router.put('/api/pet/new', validBody(newPetSchema), async (req, res, next) => {
   try {
     const pet = req.body;
     pet._id = newId();
-    debug(`insert pet`, pet)
+    debug(`insert pet`, pet);
 
     await dbModule.insertOnePet(pet);
-    res.json({ message: 'Pet Inserted.' })
+    res.json({ message: 'Pet Inserted.' });
   } catch (err) {
     next(err);
   }
